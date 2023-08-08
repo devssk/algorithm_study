68f86005e8b088df83fb0f62ade18a4698f0f234
devssk <sksong419@naver.com>
2023-08-08 11:06:18 +0900

    solved

--- src/CirclesCountry.java
+++ src/CirclesCountry.java
@@ -7,17 +7,30 @@ public class CirclesCountry {
     public static int leastBorders(int[] X, int[] Y, int[] R, int x1, int y1, int x2, int y2) {
         int answer = 0;
 
-
+        for (int i = 0; i < X.length; i++) {
+            int circleX = X[i];
+            int circleY = Y[i];
+            int circleR = R[i];
+            if (checkCircle(circleX, circleY, circleR, x1, y1)) {
+                answer++;
+            }
+            if (checkCircle(circleX, circleY, circleR, x2, y2)) {
+                answer++;
+            }
+        }
 
         return answer;
     }
 
-    public static void checkCircle(int x, int y) {
-        int X = -107;
-        int Y = 175;
-        int r = 135;
+    public static boolean checkCircle(int circleX, int circleY, int circleR, int x, int y) {
+        boolean check = false;
 
-        
+        double distance = Math.sqrt((x - circleX) * (x - circleX) + (y - circleY) * (y - circleY));
+        if (distance <= circleR) {
+            check = true;
+        }
+
+        return check;
     }
 
 }
