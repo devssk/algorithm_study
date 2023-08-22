1c22bd47bb22a07a061feb9238a329e72eb3e7e5
devssk <sksong419@naver.com>
2023-08-22 17:13:37 +0900

    init

---
+++ src/FriendScore.java
@@ -0,0 +1,27 @@
+// 여러분은 소셜 네트워크 서비스에서 가장 인기있는 사람을 찾으려고 합니다. 이를 위해 "친구의 수"를 셉니다.
+// 일단 그들이 서로 친구라면 친구입니다. 또한 C사림이 A사람과 B사람의 공통 친구라면 A사람과 B사람도 친구입니다.
+// 가장 인기 있는 사람은 친구의 수가 가장 많은 사람입니다.(가장 많은 "친구의 수"를 가진 사람은 여러 명 있을수도 있습니다.)
+// 문자열 자료형의 배열 String[] friends가 주어집니다. 만약 i번째 사람과 j번째 사람이 친구라면 i번째 요소릐 j번째 글자가 'Y'입니다.
+// 반면 친구가 아니라면 'N'입니다. 소셜 네트워크 서비스에서 가장 인기 있는 사람의 친구 수를 리턴해주세요.
+
+// friends
+// 1~50 length를 가지는 배열
+// 모든 i, j에 대해 friends[i][j] == friends[j][i]
+// 모든 i friends[i][i]는 'N' -> 자기 자신과는 친구가 아니다.
+public class FriendScore {
+    // friends[] = {"NNN", "NNN", NNN"} , 0
+    // friends[] = {"NYY", "YNY", "YYN"} , 2
+    // friends[] = {"NYNNN", "YNYNN", "NYNYN", "NNYNY", "NNNYN"}
+    // friends[] = {"NNNNYNNNNN", "NNNNYNYYNN", "NNNYYYNNNN", "NNYNNNNNNN", "YYYNNNNNNY", "NNYNNNNNYN", "NYNNNNNYNN", "NYNNNNYNNN", "NNNNNYNNNN", "NNNNYNNNNN"} , 8
+    public static void main(String[] args) {
+
+    }
+
+    public static int highestScore(String[] friends) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+}
--- src/NumberMagicEasy.java
+++ src/NumberMagicEasy.java
@@ -1,14 +1,30 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NumberMagicEasy {
     public static void main(String[] args) {
-        String answer = "YNYY";
+        String answer = "YYYY";
 
-        int i = theNumber(answer);
-        System.out.println("i = " + i);
+        theNumber(answer);
+
+        int[] card1 = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] card2 = {1, 2, 3, 4, 9, 10, 11, 12};
+        int[] card3 = {1, 2, 5, 6, 9, 10, 13, 14};
+        int[] card4 = {1, 3, 5, 7 ,9, 11, 13 ,15};
+
+        int[][] cardList = new int[4][8];
+        cardList[0] = card1;
+        cardList[1] = card2;
+        cardList[2] = card3;
+        cardList[3] = card4;
+
+        List<Integer> tempList = new ArrayList<>();
+
+        String[] split = answer.split("");
+
 
     }
 
@@ -20,69 +36,122 @@ public class NumberMagicEasy {
         int[] card3 = {1, 2, 5, 6, 9, 10, 13, 14};
         int[] card4 = {1, 3, 5, 7 ,9, 11, 13 ,15};
 
-        int[][] list = new int[4][8];
-        list[0] = card1;
-        list[1] = card2;
-        list[2] = card3;
-        list[3] = card4;
+        int[][] cardList = new int[4][8];
+        cardList[0] = card1;
+        cardList[1] = card2;
+        cardList[2] = card3;
+        cardList[3] = card4;
 
-        List<Integer> temp = new ArrayList<>();
-        boolean tempCheck = false;
+        List<Integer> list = new ArrayList<>();
 
+        for (int i = 1; i < 17; i++) {
+            list.add(i);
+        }
         String[] split = answer.split("");
         for (int i = 0; i < split.length; i++) {
-            String check = split[i];
-            if ("Y".equals(check)) {
-                int[] ints = list[i];
-                if (!tempCheck) {
-                    for (int anInt : ints) {
-                        temp.add(anInt);
-                    }
-                    tempCheck = true;
-                    continue;
+            String checkYn = split[i];
+            if ("Y".equals(checkYn)) {
+                int[] card = cardList[i];
+
+                List<Integer> copyCard = new ArrayList<>();
+                for (int number : card) {
+                    copyCard.add(number);
                 }
-                for (int j = 0; j < temp.size(); j++) {
-                    int tempInt = temp.get(j);
-                    boolean cardCheck = false;
-                    for (int k = 0; k < ints.length; k++) {
-                        int cardInt = ints[k];
-                        if (tempInt == cardInt) {
-                            cardCheck = true;
+
+                List<Integer> removeIndexList = new ArrayList<>();
+                for (int j = 0; j < list.size(); j++) {
+                    int tempNumber = list.get(j);
+                    for (int k = 0; k < copyCard.size(); k++ ) {
+                        int cardNumber = copyCard.get(k);
+                        if (tempNumber == cardNumber) {
+                            copyCard.remove(k);
+                            break;
                         }
-                    }
-                    if (!cardCheck) {
-                        temp.remove(j);
-                    }
-                }
-            } else if ("N".equals(check)) {
-                if (!tempCheck) {
-                    continue;
-                }
-                int[] ints = list[i];
-                for (int j = 0; j < temp.size(); j++) {
-                    int tempInt = temp.get(j);
-                    boolean cardCheck = false;
-                    for (int k = 0; k < ints.length; k++) {
-                        int cardInt = ints[k];
-                        if (tempInt == cardInt) {
-                            cardCheck = true;
+                        if (tempNumber != cardNumber) {
+
                         }
+
                     }
-                    if (cardCheck) {
-                        temp.remove(j);
+                }
+//                for (int j = 0; j < card.length; j++) {
+//                    int cardNumber = card[j];
+//                    for (; k < list.size(); k++) {
+//                        int tempNumber = list.get(k);
+//                        if (cardNumber == tempNumber) {
+//                            k = tempNumber;
+//                            break;
+//                        }
+//                        if (cardNumber != tempNumber) {
+//                            removeIndexList.add(k);
+//                        }
+//                    }
+//                }
+                for (int removeIndex : removeIndexList) {
+                    list.remove(removeIndex);
+                }
+            }
+            if ("N".equals(checkYn)) {
+                int[] card = cardList[i];
+                for (int j = 0; j < card.length; j++) {
+                    int cardNumber = card[j];
+                    for (int k = 0; k < list.size(); k++) {
+                        int tempNumber = list.get(k);
+                        int index = -1;
+                        if (cardNumber == tempNumber) {
+                            index = k;
+                        }
+                        if (index != -1) {
+                            list.remove(index);
+                        }
                     }
                 }
             }
-            System.out.println("Index : " + i + ", " + temp);
         }
 
-        System.out.println(temp);
-
-        if (temp.size() > 0) {
-            result = temp.get(0);
-        } else {
-            result = 16;
+        System.out.println(list);
+        if (list.size() > 0) {
+            result = list.get(0);
         }
+
+        return result;
+    }
+
+    public static int theNumber2(String answer) {
+        int result = 0;
+
+        int[] card1 = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] card2 = {1, 2, 3, 4, 9, 10, 11, 12};
+        int[] card3 = {1, 2, 5, 6, 9, 10, 13, 14};
+        int[] card4 = {1, 3, 5, 7 ,9, 11, 13 ,15};
+
+        int[][] cardList = new int[4][8];
+        cardList[0] = card1;
+        cardList[1] = card2;
+        cardList[2] = card3;
+        cardList[3] = card4;
+
+        boolean[] check = new boolean[16];
+
+        String[] split = answer.split("");
+        for (int i = 0; i < split.length; i++) {
+            String checkYn = split[i];
+            if ("Y".equals(checkYn)) {
+                int[] card = cardList[i];
+                for (int cardNumber : card) {
+                    check[cardNumber - 1] = true;
+                }
+            }
+            if ("N".equals(checkYn)) {
+                int[] card = cardList[i];
+                for (int cardNumber : card) {
+                    check[cardNumber - 1] = false;
+                }
+            }
+        }
+
+        
+
+        System.out.println(Arrays.toString(check));
 
         return result;
     }
